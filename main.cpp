783dc3ce62db537497d6fc41e4e21618a05acc47
Sergey Kormishin <s.coremission@gmail.com>
2017-05-21 15:19:03 +0300

    first attempt to make diffuse lighting

--- main.cpp
+++ main.cpp
@@ -13,11 +13,21 @@ float random() {
 	return float(rand()) / float(RAND_MAX);
 }
 
+vec3 randomInUnitSphere() {
+	vec3 p = normalize(vec3(random(), random(), random()));
+	/*do {
+		p = 2.0f * vec3(random(), random(), random()) - vec3(1.f, 1.f, 1.f);
+	} while (p.length() * p.length() >= 1.0f);
+	*/
+	return p;
+}
+
 vec3 color(const Ray& r, Hitable* world) {
 	HitRecord rec;
 
 	if(world->Hit(r, 0.0, 9999999.f, rec)) {
-		return 0.5f * (rec.normal + vec3(1.f, 1.f, 1.f));
+		vec3 target = rec.p + rec.normal + randomInUnitSphere();
+		return 0.5f * color(Ray(rec.p, target), world);
 	}
 	// gradient for background
 	vec3 unit_direction = normalize(r.direction());
@@ -25,11 +35,12 @@ vec3 color(const Ray& r, Hitable* world) {
 
 	return (1.0f - t) * vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);
 }
+void debug(){}
 
 int main(int argc, char** argv) {
 	int nx = 200;
-	int ny = 100;
-	int ns = 100;
+	int ny = 200;
+	int ns = 16;
 
 	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
@@ -38,18 +49,25 @@ int main(int argc, char** argv) {
 	list[1] = new Sphere(vec3(0.f, -100.5f, -1.f), 100);
 	Hitable* world = new HitableList(list, 2);
 	Camera cam;
-	for(int i = ny - 1; i > 0; --i) {
-		for(int j = 0; j < nx; ++j) {
+	for(int y = ny - 1; y > 0; --y) {
+		for(int x = 0; x < nx; ++x) {
 			vec3 c(0.f, 0.f, 0.f);
 			for(int s = 0; s < ns; ++s) {
-				float u = float(i + random()) / float(nx);
-				float v = float(j + random()) / float(ny);
+				if (y == 50 && x == 100) {
+					debug();
+				}
+
+				float u = float(y + random()) / float(ny);
+				float v = float(x + random()) / float(nx);
 
 				Ray r = cam.getRay(u, v);
 				c += color(r, world);
 			}
 			
 			c /= float(ns);
+			// gamma correction
+			c = vec3(sqrt(c.x), sqrt(c.y), sqrt(c.z));
+
 			int ir = int(255.99 * c.r);
 			int ig = int(255.99 * c.g);
 			int ib = int(255.99 * c.b);
